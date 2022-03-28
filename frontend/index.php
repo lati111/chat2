75a36d1347ebd16a52a93d6d97bde91e8444f97a
lati111 <jvzelst111@gmail.com>
2022-03-28 09:21:46 +0200

    front-end: added config JSON, login page and stylesheets

---
+++ frontend/index.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+$url = "";
+try {
+    $url = json_decode(file_get_contents("config.json"), true, 512, JSON_THROW_ON_ERROR)["defaultPath"] . "src/pages/chatbox.php";
+} catch (JsonException $e) {
+} echo $url;
+if (!isset($_SESSION["ID"])) {
+    header("Location: {$url}src/pages/chatbox.php");
+}
+?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>log in</title>
+    <link rel="stylesheet" href="<?php echo $url; ?>src/style/login.css">
+</head>
+
+<body>
+<div id="loginForm">
+    <div id="error"></div>
+    <h1>log in</h1>
+    <table>
+        <tr>
+            <td><span class="prepend"><b>gebruikersnaam</b></span></td>
+            <td><label><input id="usernameField" type="text" name="username"></label></td>
+        </tr>
+        <tr>
+            <td><span class="prepend"><b>wachtwoord</b></span></td>
+            <td><label><input id="passwordField" type="text" name="password"></label></td>
+        </tr>
+    </table>
+    <button id="loginButton" onclick="login()">log in</button>
+    <span><a href="register.php">of maak een account aan</a></span>
+
+
+</div>
+<script src="<?php echo $url; ?>src/scripts/login.js"></script>
+</body>
+</html>
---
+++ frontend/src/scripts/login.js
@@ -0,0 +1,61 @@
+function login() {
+    const username = document.getElementById("usernameField").value;
+    const password = document.getElementById("passwordField").value;
+    if (username === "") {
+        document.getElementById("error").textContent = "gebruikersnaam is niet ingevuld";
+        setTimeout(function() {document.getElementById("error").textContent = ""}, 2000);
+        return;
+    } else if (password === "") {
+        document.getElementById("error").textContent = "wachtwoord is niet ingevuld";
+        setTimeout(function() {document.getElementById("error").textContent = ""}, 2000);
+        return;
+    }
+
+    let http = new XMLHttpRequest();
+    let url = '../php/loginVerify.php';
+    let params = `username=${username}&password=${password}`;
+    http.open('POST', url, true);
+
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            const responseID = http.responseText;
+            if (parseInt(responseID) === 0) {
+                document.getElementById("error").textContent = "gebruikersnaam of wachtwoord incorrect";
+                setTimeout(function() {document.getElementById("error").textContent = ""}, 2000)
+            } else {
+                location.href = "../index.php";
+            }
+        }
+    }
+    http.send(params);
+}
+
+function register() {
+    const username = document.getElementById("usernameField").value;
+    const password = document.getElementById("passwordField").value;
+    if (username === "") {
+        document.getElementById("error").textContent = "gebruikersnaam is niet ingevuld";
+        setTimeout(function() {document.getElementById("error").textContent = ""}, 2000);
+        return;
+    } else if (password === "") {
+        document.getElementById("error").textContent = "wachtwoord is niet ingevuld";
+        setTimeout(function() {document.getElementById("error").textContent = ""}, 2000);
+        return;
+    }
+
+    let http = new XMLHttpRequest();
+    let url = '../php/regUser.php';
+    let params = `username=${username}&password=${password}`;
+    http.open('POST', url, true);
+
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            location.href = "../index.php";
+        }
+    }
+    http.send(params);
+}
